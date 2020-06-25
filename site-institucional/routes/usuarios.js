766c04551a17514e0f2d7e68bbcfbf08a77e077a
lucasmessiasbandtec <lucas.messias@bandtec.com.br>
2020-06-24 22:06:04 -0300

    remove gitignore

--- site-institucional/routes/usuarios.js
+++ site-institucional/routes/usuarios.js
@@ -11,52 +11,36 @@ router.get('/', function(req, res, next) {
 // let sessoes = [];
 
 /* Recuperar usuário por login e senha */
-// router.post('/autenticar', function(req, res, next) {
-// 	console.log('Recuperando usuário por login e senha');
+router.post('/usuarios/autenticar', function(req, res, next) {
+	console.log('Recuperando usuário por login e senha');
 
-// 	var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
-// 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
+	var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
-// 	let instrucaoSql = `select * from CONTRATANTE where email='${login}' and senha='${senha}'`;
-// 	console.log(instrucaoSql);
+	let instrucaoSql = `select * from CONTRATANTE where email='${login}' and senha='${senha}'`;
+	console.log(instrucaoSql);
 
-// 	sequelize.query(instrucaoSql, {
-// 		model: Usuario
-// 	}).then(resultado => {
-// 		console.log(`Encontrados: ${resultado.length}`);
+	sequelize.query(instrucaoSql, {
+		model: Usuario
+	}).then(resultado => {
+		console.log(`Encontrados: ${resultado.length}`);
 
-// 		if (resultado.length == 1) {
-// 			sessoes.push(resultado[0].dataValues.login);
-// 			console.log('sessoes: ',sessoes);
-// 			res.json(resultado[0]);
-// 		} else if (resultado.length == 0) {
-// 			res.status(403).send('Login e/ou senha inválido(s)');
-// 		} else {
-// 			res.status(403).send('Mais de um usuário com o mesmo login e senha!');
-// 		}
+		if (resultado.length == 1) {
+			sessoes.push(resultado[0].dataValues.login);
+			console.log('sessoes: ',sessoes);
+			res.json(resultado[0]);
+		} else if (resultado.length == 0) {
+			res.status(403).send('Login e/ou senha inválido(s)');
+		} else {
+			res.status(403).send('Mais de um usuário com o mesmo login e senha!');
+		}
 
-// 	}).catch(erro => {
-// 		console.error(erro);
-// 		res.status(500).send(erro.message);
-//   	});
-// });
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+  	});
+});
 
-/* Cadastrar usuário */
-// router.post('/cadastrar', function(req, res, next) {
-// 	console.log('Criando um usuário');
-	
-// 	Usuario.create({
-// 		nome : req.body.nome,
-// 		login : req.body.login,
-// 		senha: req.body.senha
-// 	}).then(resultado => {
-// 		console.log(`Registro criado: ${resultado}`)
-//         res.send(resultado);
-//     }).catch(erro => {
-// 		console.error(erro);
-// 		res.status(500).send(erro.message);
-//   	});
-// });
 
 router.post('/cadastrar', (req, res, next) => {
 
